17a08eac821eb514ed9bb20cbd68511aee5b418e
ThamanGurung01 <tgurung519@gmail.com>
2024-09-16 18:41:27 +0545

    small change

--- frontend/src/components/viewTable.tsx
+++ frontend/src/components/viewTable.tsx
@@ -25,14 +25,13 @@ const ViewTable:React.FC<viewTableProps> = ({value}) => {
   useEffect(()=>{getRequest();},[value]);
   return (
     <div className=''>
-      <legend className='text-center mt-5 pb-5 text-2xl text-white font-bold'>{value.toUpperCase()}</legend>
+      <legend className='text-center mt-5 pb-5 text-2xl font-bold'>{value.toUpperCase()}</legend>
       <table className='table-auto tableStyle border-collapse border border-red-600'>
     <thead>
 <tr>
 <th className='tableElementBorder tableStyleRow'>Amount</th>
       <th className='tableElementBorder tableStyleRow'>Category</th>
       <th className='tableElementBorder tableStyleRow'>Date</th>
-      {/* <th className='tableElementBorder tableStyleRow' colSpan={2}>Action</th> */}
 </tr>
     </thead>
     <tbody>
@@ -40,8 +39,7 @@ const ViewTable:React.FC<viewTableProps> = ({value}) => {
 <td className='tableElementBorder tableStyleRow' title={el[value+"_description"]}>{el[value+"_amount"]}</td>
 <td className='tableElementBorder tableStyleRow' title={el[value+"_description"]}>{el[value+"_category"]}</td>
 <td className='tableElementBorder tableStyleRow' title={el[value+"_description"]}>{el[value+"_date"]}</td>
-{/* <td className='tableElementBorder tableStyleRow'><Link to={"/update/"+el["_id"]+"/"+value}>Update</Link></td>
-<td className='tableElementBorder tableStyleRow'><button onClick={()=>handleDelete(el["_id"])}>Delete</button></td> */}
+
 </tr>):( <tr key={i}><td className='tableElementBorder text-center' colSpan={4}>No data found</td></tr> )
  )}
     </tbody>
